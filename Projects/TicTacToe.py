21a045fe6c38e29138a726acc9b2cace7df2dd29
jck14 <jonathan.kapene@gmail.com>
2018-08-19 09:59:40 +1245

    Refactor function names

--- Projects/TicTacToe.py
+++ Projects/TicTacToe.py
@@ -1,20 +1,20 @@
-def DisplayBoard(b):
+def display_board(b):
     print(f"{b[7]} | {b[8]} | {b[9]}")
     print(f"{b[4]} | {b[5]} | {b[6]}")
     print(f"{b[1]} | {b[2]} | {b[3]}")
 
 
-def VictoryCheck(b, player):
+def victory_check(b, player):
     return b[1] == b[2] == b[3] == player or b[4] == b[5] == b[6] == player or b[7] == b[8] == b[9] == player\
 or b[1] == b[4] == b[7] == player or b[2] == b[5] == b[8] == player or b[3] == b[6] == b[9] == player\
 or b[7] == b[5] == b[3] == player or b[1] == b[5] == b[9] == player
 
 
-def StartingMenu():
+def starting_menu():
     input("Press Enter to play!")
 
 
-def PlayerMove(b, player):
+def player_move(b, player):
 
     while True:
         move = int(input("Choose pos: "))
@@ -33,25 +33,24 @@ if __name__ == '__main__':
 
     gameBoard = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-
     while True:
-        StartingMenu()
+        starting_menu()
 
         while True:
-            DisplayBoard(gameBoard)
+            display_board(gameBoard)
 
-            if VictoryCheck(gameBoard, 'O'):
+            if victory_check(gameBoard, 'O'):
                 print("Winner: 'O'")
                 break
 
-            gameBoard = PlayerMove(gameBoard, 'X')
+            gameBoard = player_move(gameBoard, 'X')
 
-            DisplayBoard(gameBoard)
+            display_board(gameBoard)
 
-            if VictoryCheck(gameBoard, 'X'):
+            if victory_check(gameBoard, 'X'):
                 print("Winner: 'X'")
                 break
 
-            gameBoard = PlayerMove(gameBoard, 'O')
+            gameBoard = player_move(gameBoard, 'O')
 
 
